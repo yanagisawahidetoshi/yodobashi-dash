0525f4f395e565ea4d68f92855cac2f5d94f39ba
HidetoshiYanagisawa <yanagisawa@ultrasevenstar.com>
2017-10-31 09:34:27 +0000

    app.jsリファクタ

--- app.js
+++ app.js
@@ -1,60 +1,49 @@
-var http = require('http');
-var fs   = require('fs');
-var qs   = require("querystring");
-var exec = require('child_process').exec;
+const http = require('http'),
+        fs   = require('fs'),
+        qs   = require("querystring"),
+        exec = require('child_process').exec,
+        config = require('./config/config'),
+        urls   = require('./config/urls'),
+        sqlite = require('./modules/db.js'),
+        db       = sqlite.init('./db/yodobashi.sqlite3'),
+        crypto   = require('./modules/crypto.js');
 
-var config = require('./config/config');
-var urls   = require('./config/urls');
 
-const sqlite = require('./modules/db.js'),
-    db       = sqlite.init('./db/yodobashi.sqlite3')
-    crypto   = require('./modules/crypto.js');
-    
-
-http.createServer(function (req, res) {
-    if(req.url === '/' && req.method === 'GET') {
-        fs.readFile(__dirname + '/index.html', {
-            encoding: 'utf8'
-        }, function(err, html) {
-            if (err) {
-                res.statusCode = 500;
-                res.end('Error!');
-            }else {
-                res.setHeader('Content-Type', 'text/html');
-                res.end(html);
-            }
-        });
-    }else if (req.url === '/' && req.method === 'POST') {
-        var body='';
-        req.on('data', function (data) {
-            body +=data;
-        });
-        req.on('end',function(){
-            var post = qs.parse(body);
+http.createServer((req, res) => {
+    if (req.url === '/' && req.method === 'POST') {
+        let body = '';
+        req.on('data', data => body += data);
+        req.on('end', () => {
+            const post = qs.parse(body);
             
-            if(config.apiKey !== post.apiKey){
-                console.log('APIKEYが違います');
-                return;
-            }
+            new Promise((resolve, reject) => {
+                if(config.apiKey !== post.apiKey){
+                    reject('APIKEYが違います');
+                }
+                
+                if(! urls[post.key]) {
+                    reject('urlが見つかりません');
+                }
             
-            if(! urls[post.key]) {
-                console.log('urlが見つかりません');
-                return
-            }
-            
-            
-            db.get('select * from users where id = 1', (err, res) => {
-                if (err) return;
+                db.get('select * from users where id = 1', (err, res) => resolve(res));
+            }).then(res => {
                 res.password      = crypto.decrypt(res.password);
                 res.security_code = crypto.decrypt(res.security_code);
                 res.url = urls[post.key];
-
-                exec("casperjs yodobashi.js '" + JSON.stringify(res) + "'", (err, stdout, stderr) => {
-                    if (err) { console.log(err); }
-                    console.log(stdout);
+                
+                return new Promise((resolve, reject) => {
+                    exec("casperjs yodobashi.js '" + JSON.stringify(res) + "'", (err, stdout, stderr) => {
+                        if(err) { 
+                            reject(err);
+                        }
+                        console.log(stdout);
+                    });
                 });
+            }).catch(err => {
+                console.log(err);
+            }).then(function(){
+                res.end();
             });
-            res.end();
         });
     }
 }).listen(process.env.PORT || 8000);
